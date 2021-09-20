5529a6d43dbae70c51dc9123b64d7a5e920f4f7f
IRUY <a36243845@gmail.com>
2021-09-21 02:35:40 +0900

    floatingbutton mk

--- app/src/main/java/kr/co/iruy/car_ing/MainActivity.kt
+++ app/src/main/java/kr/co/iruy/car_ing/MainActivity.kt
@@ -1,5 +1,6 @@
 package kr.co.iruy.car_ing
 
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
@@ -17,11 +18,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import androidx.viewpager.widget.ViewPager
-import com.google.android.material.tabs.TabLayout
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.textfield.TextInputEditText
 
-class SplashActivity: AppCompatActivity(){
+class SplashActivity : AppCompatActivity() {
     private val time: Long = 1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +38,7 @@ class SplashActivity: AppCompatActivity(){
 }
 
 class LoginActivity : AppCompatActivity() {
-    private val caringImageview : ImageView by lazy{
+    private val caringImageview: ImageView by lazy {
         findViewById<ImageView>(R.id.caringImageview)
     }
     private val signUpButton: Button by lazy {
@@ -57,7 +58,7 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         // 로고 색상 변경
-        caringImageview.setColorFilter(Color.parseColor("#FFE816"),PorterDuff.Mode.SRC_IN)
+        caringImageview.setColorFilter(Color.parseColor("#FFE816"), PorterDuff.Mode.SRC_IN)
         // 회원 가입 클릭시
         signUpButton.setOnClickListener {
             val intent = Intent(this, signUpActivity::class.java)
@@ -114,6 +115,7 @@ class signUpActivity : AppCompatActivity() {
             }
         })
     }
+
     fun checkEmail(): Boolean {
         var getEmail = tietemail.text.toString().trim()
         if (emailValid.matches(getEmail)) {
@@ -129,23 +131,66 @@ class signUpActivity : AppCompatActivity() {
 }
 
 class MainActivity : AppCompatActivity() {
-    var tsImgRes = intArrayOf(
-        R.drawable.bom1,R.drawable.bom1,R.drawable.bom1,R.drawable.bom1,
-        R.drawable.bom1,R.drawable.bom1,R.drawable.bom1,R.drawable.bom1
-    )
-    var tsTag = arrayOf("태그","태그","태그","태그","태그","태그","태그","태그")
-    var tsTitle = arrayOf("제목","제목","제목","제목","제목","제목","제목","제목")
+    private val fabList : FloatingActionButton by lazy{
+        findViewById<FloatingActionButton>(R.id.fabList)
+    }
+    private val fabMsg : FloatingActionButton by lazy{
+        findViewById<FloatingActionButton>(R.id.fabMsg)
+    }
+    private val fabCustom : FloatingActionButton by lazy{
+        findViewById<FloatingActionButton>(R.id.fabCustom)
+    }
+    private var isFabOpen = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 화면 전환
-        val pagerAdapter = FragmentAdapter(supportFragmentManager)
-        val pager = findViewById<ViewPager>(R.id.viewPager)
-        pager.adapter = pagerAdapter
-        val tab = findViewById<TabLayout>(R.id.tab)
-        tab.setupWithViewPager(pager)
+        fabList.setOnClickListener{
+            toggleFab()
+        }
+        //하단 버튼
+        var bnv = findViewById(R.id.bnv) as BottomNavigationView
+        bnv.run { setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.homeBT -> {
+                    // 다른 프래그먼트 화면으로 이동하는 기능
+                    val homeFragment = HomeFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, homeFragment).commit()
+                }
+                R.id.storeBT -> {
+                    val storeFragment = StoreFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, storeFragment).commit()
+                }
+                R.id.messageBT -> {
+                    val messageFragment = MessageFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, messageFragment).commit()
+                }
+                R.id.garageBT -> {
+                    val garageFragment = GarageFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, garageFragment).commit()
+                }
+                R.id.profileBT -> {
+                    val profileFragment = ProfileFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, profileFragment).commit()
+                }
+            }
+            true
+        }
+            selectedItemId = R.id.homeBT
+        }
+    }
 
+    private fun toggleFab(){
+        if (isFabOpen){
+            ObjectAnimator.ofFloat(fabCustom,"translationY",0f).apply { start() }
+            ObjectAnimator.ofFloat(fabMsg,"translationY",0f).apply { start() }
+            isFabOpen = false
+        }
+        else {
+            ObjectAnimator.ofFloat(fabCustom,"translationY",-175f).apply { start() }
+            ObjectAnimator.ofFloat(fabMsg,"translationY",-300f).apply { start() }
+            isFabOpen = true
+        }
     }
     // 뒤로 가기 버튼 막기
     override fun onBackPressed() {
@@ -153,11 +198,10 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-class FragmentAdapter (fm : FragmentManager): FragmentPagerAdapter(fm) {
+class FragmentAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
     // 포지션 따라 이동
     override fun getItem(position: Int): Fragment {
-        val fragment = when(position)
-        {
+        val fragment = when (position) {
             0 -> RecommendFragment.newInstant()
             1 -> PlazaFragment.newInstant()
             2 -> EditorFragment.newInstant()
@@ -171,8 +215,7 @@ class FragmentAdapter (fm : FragmentManager): FragmentPagerAdapter(fm) {
     override fun getCount(): Int = 5
 
     override fun getPageTitle(position: Int): CharSequence? {
-        val title = when(position)
-        {
+        val title = when (position) {
             0 -> "맞춤추천"
             1 -> "광장"
             2 -> "에디터"
