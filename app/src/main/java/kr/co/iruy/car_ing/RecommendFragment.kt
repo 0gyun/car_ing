4b2f2ac6b97935c1f678209f531c8bf9245802a5
YoungGyunLee <yklee981130@gmail.com>
2021-10-03 15:59:09 +0900

    1. 추천 fragment에서 오늘의 카잉 부분을 인스타 게시글처럼 이미지를 넘길 수 있게 한 번 만들어 봄. 하지만 여러 개를 하기 위해서는 데이터 베이스가 필수적이라 보고 현재 단계에서는 직접 여러 개를 삽입하는 방법으로 실행함. 2. RecommendFragment.kt에서 adapter를 사용하기 위해 ViewPagerAdapter.kt를 이용했다.

--- app/src/main/java/kr/co/iruy/car_ing/RecommendFragment.kt
+++ app/src/main/java/kr/co/iruy/car_ing/RecommendFragment.kt
@@ -5,13 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import kr.co.iruy.car_ing.Adapter.ViewPagerAdapter
+
 class RecommendFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recommend, container, false)
+        // fragment_recommend를 inflate하고 그 안에 ViewPager를 adapter를 통해 image 넘길 수 있게
+        val view = inflater.inflate(R.layout.fragment_recommend, container, false)
+        val viewPager = view.findViewById<ViewPager>(R.id.view_pager)
+        val adapter = ViewPagerAdapter(requireContext())
+        viewPager.adapter = adapter
+        return view
     }
     companion object{
         fun newInstant() : RecommendFragment
