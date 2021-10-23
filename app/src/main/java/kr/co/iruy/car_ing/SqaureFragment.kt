ecc7522805da028ef9fd759c7d6eae40834c09cf
YoungGyunLee <yklee981130@gmail.com>
2021-10-23 12:01:50 +0900

    conflicts end?

--- app/src/main/java/kr/co/iruy/car_ing/SqaureFragment.kt
+++ app/src/main/java/kr/co/iruy/car_ing/SqaureFragment.kt
@@ -1,12 +1,9 @@
 package kr.co.iruy.car_ing
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 
 class SqaureFragment : Fragment() {
@@ -15,13 +12,7 @@ class SqaureFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val v : View = inflater.inflate(R.layout.fragment_square,container,false)
-        val squareCV = v.findViewById<CardView>(R.id.squareCV)
-        squareCV.setOnClickListener {
-            val intent = Intent(activity, FeedActivity::class.java)
-            startActivity(intent)
-        }
-        return v
+        return inflater.inflate(R.layout.fragment_square, container, false)
     }
     companion object{
         fun newInstant() : SqaureFragment
