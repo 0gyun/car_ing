a1181ceea32dcf7a8fff0979f3640b01cdc96dd4
IRUY <a36243845@gmail.com>
2021-11-13 17:03:36 +0900

    bookkkk

--- app/src/main/java/kr/co/iruy/car_ing/Book2Activity.kt
+++ app/src/main/java/kr/co/iruy/car_ing/Book2Activity.kt
@@ -1,11 +1,18 @@
 package kr.co.iruy.car_ing
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Book2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book2)
+        val book3BT = findViewById<Button>(R.id.book3BT)
+        book3BT.setOnClickListener{
+            val Intent = Intent(this, Book3Activity::class.java)
+            startActivity(Intent)
+        }
     }
 }
