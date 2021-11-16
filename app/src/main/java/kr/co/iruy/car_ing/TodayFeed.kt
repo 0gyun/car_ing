4d209ee1b4de512e5af23a36744f20bfa546f6b7
YoungGyunLee <yklee981130@gmail.com>
2021-11-16 22:13:19 +0900

    아이템 정보 띄우는 기능

--- app/src/main/java/kr/co/iruy/car_ing/TodayFeed.kt
+++ app/src/main/java/kr/co/iruy/car_ing/TodayFeed.kt
@@ -2,8 +2,10 @@ package kr.co.iruy.car_ing
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class TodayFeed : AppCompatActivity(){
@@ -15,6 +17,25 @@ class TodayFeed : AppCompatActivity(){
         val back_btn = findViewById<ImageButton>(R.id.today_back_btn)
         back_btn.setOnClickListener{
             finish()
+        }
+
+        var info_on : Boolean = false
+        val item_info_btn = findViewById<ImageButton>(R.id.today_item_info_button)
+        val item_info_1 = findViewById<ImageView>(R.id.item_info1)
+        val item_info_2 = findViewById<ImageView>(R.id.item_info2)
+        item_info_btn.setOnClickListener{
+            if(info_on){
+                item_info_btn.setImageResource(R.drawable.ic_hide_item)
+                item_info_1.visibility = View.INVISIBLE
+                item_info_2.visibility = View.INVISIBLE
+                info_on = false
+            }
+            else{
+                item_info_btn.setImageResource(R.drawable.ic_show_item)
+                item_info_1.visibility = View.VISIBLE
+                item_info_2.visibility = View.VISIBLE
+                info_on = true
+            }
         }
     }
 
